70af7006031925050b4a5d299aa2e68604be466c
Official-saran <165557929+Official-saran@users.noreply.github.com>
2024-06-11 21:07:42 +0530

    Delete discountPrice.py

--- discountPrice.py
+++
@@ -1,9 +0,0 @@
-marked_price = float(input("Enter the marked price: \n"))
-if marked_price > 10000:
-    discount = 0.20
-elif marked_price > 7000:
-    discount = 0.15
-else:
-    discount = 0.10
-net_amount = marked_price - (discount * marked_price)
-print(f"The net amount to pay after discount is: {net_amount:.2f}")
