6ce922611ee542f2218df071437c13192fff9b6e
Official-saran <165557929+Official-saran@users.noreply.github.com>
2024-06-11 21:07:08 +0530

    Delete addStringInMiddleOfInputString.py

--- addStringInMiddleOfInputString.py
+++
@@ -1,5 +0,0 @@
-str1 = input("Enter the first string (str1): ")
-str2 = input("Enter the second string (str2): ")
-middle_index = len(str1) // 2
-str3 = str1[:middle_index] + str2 + str1[middle_index:]
-print("New string:", str3)
